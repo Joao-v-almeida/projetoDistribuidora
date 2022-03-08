391a6f34c30850a34d3f517116e6e2092e1beb5d
João Almeida <jaovittor.almeida@gmail.com>
2022-03-08 19:30:37 -0300

    Teste

--- src/controller/ControllerCliente.java
+++ src/controller/ControllerCliente.java
@@ -8,7 +8,7 @@ import javax.swing.JOptionPane;
 import DAO.DaoCliente;
 import model.ModelCliente;
 
-public class ControllerCliente {
+public class ControllerCliente { //TESTE JOÃO
 	private DaoCliente daoCliente = new DaoCliente();
 
 	public int salvarClienteController(ModelCliente modelCliente) {
